3ebf77d5d3169ded939f5dcf36271b6c66aab8e0
Kawane Serra <kawane.serra@hotmail.com>
2022-07-14 08:53:39 -0300

    API test

---
+++ backend/src/main/java/com/devsuoperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuoperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuoperior.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
